87bf3d4d783f71f378369d54fdabd498feac7e20
unknown <lobbyc@LobbyC-PC.hhtowers.local>
2019-11-30 05:28:57 +0200

    Finally

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, Provider } from '@angular/core';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { registerLocaleData } from '@angular/common';
-import ruLocale from '@angular/common/locales/ru';
-import enLocale from '@angular/common/locales/en';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, Provider } from "@angular/core";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { registerLocaleData } from "@angular/common";
+import ruLocale from "@angular/common/locales/ru";
+import enLocale from "@angular/common/locales/en";
+import { ServiceWorkerModule } from "@angular/service-worker";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { PostPageComponent } from './post-page/post-page.component';
-import { PostComponent } from './shared/components/post/post.component';
-import { SharedModule } from './shared/shared.module';
-import { AuthInterceptor } from './shared/auth.interceptor';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MainLayoutComponent } from "./shared/components/main-layout/main-layout.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { PostPageComponent } from "./post-page/post-page.component";
+import { PostComponent } from "./shared/components/post/post.component";
+import { SharedModule } from "./shared/shared.module";
+import { AuthInterceptor } from "./shared/auth.interceptor";
+import { environment } from "../environments/environment";
 
-registerLocaleData(ruLocale, 'ru')
-registerLocaleData(enLocale, 'en')
+registerLocaleData(ruLocale, "ru");
+registerLocaleData(enLocale, "en");
 
 const INTERCEPTOR_PROVIDER: Provider = {
   provide: HTTP_INTERCEPTORS,
   multi: true,
   useClass: AuthInterceptor
-}
+};
 
 @NgModule({
   declarations: [
@@ -34,9 +36,12 @@ const INTERCEPTOR_PROVIDER: Provider = {
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production
+    })
   ],
   providers: [INTERCEPTOR_PROVIDER],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
